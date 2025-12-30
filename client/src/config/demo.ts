14cdd8741ab1c0ea73f0313a859669ed2de9867a
VittorioC13 <vche@wesleyan.edu>
2025-12-30 18:56:33 +0800

    Add yukino4.jpg as fourth demo panel

--- client/src/config/demo.ts
+++ client/src/config/demo.ts
@@ -26,5 +26,9 @@ export const DEMO_ITEMS: DemoItem[] = [
     imageUrl: "/demo/yukino3.jpg",
     plyUrl: "", // Will auto-generate on click
   },
+  {
+    imageUrl: "/demo/yukino4.jpg",
+    plyUrl: "", // Will auto-generate on click
+  },
 ];
 
