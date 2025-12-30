ed3770f4e4536e80a67abbe4a180881c99d96e0b
VittorioC13 <vche@wesleyan.edu>
2025-12-30 17:56:24 +0800

    Add 7 new demo images to demo config

--- client/src/config/demo.ts
+++ client/src/config/demo.ts
@@ -16,8 +16,35 @@ export interface DemoItem {
 export const DEMO_ITEMS: DemoItem[] = [
   {
     imageUrl: "/demo/yukino.jpg",
-    plyUrl: "", // Will be set after generation
+    plyUrl: "", // Will auto-generate on click
   },
-  // Add more demo items here when ready
+  {
+    imageUrl: "/demo/1c190a2383579dd8677b1800cc37ff50.jpg",
+    plyUrl: "", // Will auto-generate on click
+  },
+  {
+    imageUrl: "/demo/2984ce593957345586ff4f3363105438.jpg",
+    plyUrl: "", // Will auto-generate on click
+  },
+  {
+    imageUrl: "/demo/361f97d61c720cbb64f00315b215f1ce.jpg",
+    plyUrl: "", // Will auto-generate on click
+  },
+  {
+    imageUrl: "/demo/a1b9ac03d9ecc4cddc9f036f79eb8b98.jpg",
+    plyUrl: "", // Will auto-generate on click
+  },
+  {
+    imageUrl: "/demo/aa94c15c77636d395d31a6809a626ca3.jpg",
+    plyUrl: "", // Will auto-generate on click
+  },
+  {
+    imageUrl: "/demo/dc8ebde3a7432c297d89cb5c4bdedb15.jpg",
+    plyUrl: "", // Will auto-generate on click
+  },
+  {
+    imageUrl: "/demo/ecabe19e42eb1f777aca49b95a4228cd.jpg",
+    plyUrl: "", // Will auto-generate on click
+  },
 ];
 
